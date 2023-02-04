9b6914cf208d0dc3fd6e5dd39558eb2f0b6ce240
Joel7Anthony <anto777molina777@gmail.com>
2023-02-03 20:35:16 -0500

    acabamos XD

---
+++ js/swetA.js
@@ -0,0 +1,8 @@
+Swal.fire({
+    title: 'Gracias!',
+    text: 'Esto es un poco de nosotros un gusto.',
+    imageUrl: 'https://www.prensalibre.com/wp-content/uploads/2023/01/messi-cristiano-ronaldo.jpg?quality=52',
+    imageWidth: 400,
+    imageHeight: 200,
+    imageAlt: 'Custom image',
+  })
